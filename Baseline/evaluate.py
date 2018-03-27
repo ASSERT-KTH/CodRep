b5d8c517a277151d39304a3b34a6a12c0f91b9ed
Zimin Chen <zimin@Zimins-MacBook-Pro.local>
2018-03-27 16:02:56 +0200

    Updated evaluate.py to init maximum loss to all files in the beginning. Means that if the algorithm did not predict anything, we assume maximum loss

--- Baseline/evaluate.py
+++ Baseline/evaluate.py
@@ -1,12 +1,12 @@
 import sys
+import glob
 import math
 
 class LineOutOfRangeException(Exception):
     pass
 
 def lossFunction(guess, solution):
-    #return abs(solution-guess)
-    return math.tanh(0.3*abs(solution-guess))
+    return math.tanh(abs(solution-guess))
 
 def main():
     k = 0 # Maximum number of solutions received
@@ -14,9 +14,16 @@ def main():
     path_files = "../Files/"
     path_solutions = "../Solutions/"
 
-    total_files = 0
+    # All txt files, ignore other file extension such as hidden files
+    all_files = glob.glob1(path_files, "*.txt")
+
+    total_files = len(all_files)
     correct_files = 0
-    loss = 0
+
+    # Init loss of each file as maximum, x ->inf, tanh(x) -> 1
+    score = {}
+    for filename in all_files:
+        score[filename] = 1
 
     for args in sys.stdin:
         inputs = args.split()
@@ -33,13 +40,16 @@ def main():
             # Ignore first two lines since they are not part of the program
             code_file_length = len(code_file.readlines())-2
 
+            # Check if answers are in range
             for answer in answers:
                 if(answer < 1 or answer > code_file_length):
                     raise LineOutOfRangeException("Line number out of range for file " + filename + "." +
                     " Expected: 1<={line}<=" + str(code_file_length) + ", found: " + answer)
 
+            # Read the solution
             solution = solution_file.readline()
             solution = int(solution)
+
             # Use minimum loss among all answers to calculate loss
             min_loss = float('Inf')
             for answer in answers:
@@ -50,8 +60,8 @@ def main():
                     correct_files+=1
                     break
 
-            total_files+=1
-            loss+=min_loss
+            # Update the new score
+            score[filename]=min_loss
 
             code_file.close()
             solution_file.close()
@@ -61,9 +71,10 @@ def main():
         except IOError:
             print(filename + " does not exist!")
             raise
-
+    print(str(sum(score.values())))
+    print(str(total_files))
     print("Total files: " + str(total_files))
-    print("Average line error: " + str(loss/(total_files*1.0)) + " (the lower, the better)")
+    print("Average line error: " + str(sum(score.values())/(total_files*1.0)) + " (the lower, the better)")
     print("Top " + str(k) + " accuracy: " + str(correct_files/(total_files*1.0)) + " (the higher, the better)")
 
 if __name__=="__main__":
